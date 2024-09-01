4734deccf2a5fa488358d620960dcda570046339
lopesdaluz <tlopesdaluz@gmail.com>
2024-09-01 16:38:09 +0200

    first commit

---
+++ src/Components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Tatiana</h1>
+      {/* <button></button> */}
+    </header>
+  );
+}
