d5d692c90476ba675592cc93f02f64ff2d45eb6a
lopesdaluz <tlopesdaluz@gmail.com>
2024-09-08 17:25:38 +0200

    fixed netflify email

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -7,8 +7,10 @@ export default function contact() {
         <h2>Contact Me</h2>
       </div>
       <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
+        <input type="hidden" id="name" name="name" required />
 
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" name="email" required />
